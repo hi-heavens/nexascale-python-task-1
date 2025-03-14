3f35dcc79b69f37cc3254fbe1110570f4c2b1f7f
Olaitan Kehinde <ken_adex@yahoo.com>
2025-03-14 22:26:43 +0100

    print CPU and memory usage after every 5 seconds

--- monitor-script.py
+++ monitor-script.py
@@ -1,13 +1,18 @@
 import psutil
 import time
 
-# memory_usage
+
+seconds = 5
 
 while True:
     # This will print the CPU usage in percentage
     cpu_usage = psutil.cpu_percent()
 
-    print(f"CPU Usage: {cpu_usage}%")
+    # This will print the memory usage in percentage
+    memory_usage = psutil.virtual_memory().percent
+
+    print(f"CPU Usage: {cpu_usage}% Memory Usage: {memory_usage}")
+    # print(f"CPU Usage: {cpu_usage}%")
 
     # time interval for the loop
-    time.sleep(1)
+    time.sleep(seconds)
