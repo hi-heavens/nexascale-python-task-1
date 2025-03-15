923ef2e086286dae8adbef02f77890a1354eca64
Olaitan Kehinde <ken_adex@yahoo.com>
2025-03-15 14:39:57 +0100

    filter by severity level

--- 03-log-scanner.py
+++ 03-log-scanner.py
@@ -1,11 +1,13 @@
-def error_scanner(file_name):
+def error_scanner(file_name, level='ERROR'):
     with open(file_name, 'r') as f:
         count = 0
         for line in f:
-            if 'ERROR' in line:
+            if level in line:
                 count += 1
-        print(f"Found {count} occurrences of 'ERROR' in logs.")
+        print(f"Found {count} occurrences of {level} in logs.")
 
 if __name__ == '__main__':
     file_name = 'application.log'
     error_scanner(file_name)
+    error_scanner(file_name, 'INFO')
+    error_scanner(file_name, 'WARNING')
